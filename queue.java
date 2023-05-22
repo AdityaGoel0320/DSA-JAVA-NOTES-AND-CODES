a6e2688a03d517ece38df40ac81afc32fe4f416b
Aditya Goel <adityagoelgithub0320@gmail.com>
2023-05-22 15:12:36 +0530

    22/05/23

--- queue.java
+++ queue.java
@@ -45,9 +45,20 @@ public class queue {
         for (int i = 1; i <= 5; i++) {
             s1.push(i);
         }
-        System.out.println(s1.pop());
-        System.out.println(s1.pop());
-        System.out.println(s1.peek());
+        // System.out.println(s1.pop());
+        // System.out.println(s1.pop());
+        // System.out.println(s1.peek());
+
+        ArrayList<Integer> a1 = new ArrayList<>();
+
+        a1.add(1);
+        a1.add(2);
+        a1.add(1);
+        a1.add(1);
+        a1.add(1);
+        System.out.println(a1);
+        System.out.println(a1.get(1));
+        System.out.println(a1.get(0));
     }
 
 }
