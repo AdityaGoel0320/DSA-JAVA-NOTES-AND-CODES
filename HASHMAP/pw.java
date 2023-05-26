c515c6e86a9db1edc2839f28e05b13a6828b1a96
Aditya Goel <adityagoelgithub0320@gmail.com>
2023-05-26 21:15:09 +0530

    26/05/23

---
+++ HASHMAP/pw.java
@@ -0,0 +1,17 @@
+package HASHMAP;
+
+import java.util.HashMap;
+
+public class pw {
+    public static void main(String[] args) {
+        HashMap<String, Integer> freq = new HashMap<>();
+
+        int[] arr = { 1,1,1,1,1,1,1,1,1,1,2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5 };
+
+        for(injt )
+
+
+
+    }
+
+}
