5f989d95b25c295a8d5ed4efe3fd4c82819b6810
Aditya Goel <adityagoelgithub0320@gmail.com>
2023-06-03 23:27:09 +0530

    03/06/23

--- HASHMAP/pw.java
+++ HASHMAP/pw.java
@@ -3,17 +3,37 @@ package HASHMAP;
 import java.util.HashMap;
 
 public class pw {
-    public static void main(String[] args) {
-        HashMap<String, Integer> h1 = new HashMap<>();
+    static HashMap<Character, Integer> makeFreqMap(String str) {
+        HashMap<Character, Integer> h1 = new HashMap<>();
 
+        for (int i = 0; i <= str.length() - 1; i++) {
+            Character c1 = str.charAt(i);
 
+            if (h1.containsKey(c1)) {
+                int freq = h1.get(c1);
+                h1.put(c1, freq + 1);
+            } else {
+                h1.put(c1, 1);
 
+            }
+        }
+        return h1;
     }
 
+    static boolean anagram(String str1, String str2) {
+        HashMap<Character, Integer> h1 = makeFreqMap(str1);
+        HashMap<Character, Integer> h2 = makeFreqMap(str2);
 
+        return h1.equals(h2);
+    }
 
+    public static void main(String[] args) {
 
+        System.out.println(
+                anagram("knee", "")
 
-    
+        );
+
+    }
 
 }
--- Kunal.java
+++
@@ -1,6 +0,0 @@
-public class Kunal {
-    public static void main(String[] args) {
-        System.out.println("kunal folder");
-    }
-
-}
